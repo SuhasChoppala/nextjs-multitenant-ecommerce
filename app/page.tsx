c98a40fa3561c8c69040c322ba7839cf7b838fb4
SuhasChoppala <choppalasuhas.02@gmail.com>
2026-02-19 13:31:17 +0530

    customised shadcn components and theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello World</p>
+    <div className="flex flex-col space-y-4 p-4">
+      <div>
+        <Button variant="elevated">i AM A button</Button>
+      </div>
+      <div>
+        <Input placeholder="i am an input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea value="I am a text area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
