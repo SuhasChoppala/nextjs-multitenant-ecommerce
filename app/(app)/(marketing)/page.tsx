27d2b5e85f602747d927487fcfddae0bc8bc14e2
SuhasChoppala <choppalasuhas.02@gmail.com>
2026-02-20 16:18:18 +0530

    integrated payload

---
+++ app/(app)/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  return <div></div>;
+}
